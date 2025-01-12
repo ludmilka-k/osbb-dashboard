ba2285bdd40a6ae22ef1c137984cdfea073a1322
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-01-12 15:57:56 +0200

    Add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,10 @@
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: 'Customers',
+};
 
 export default async function CustomersPage(props: {
     searchParams?: Promise<{
