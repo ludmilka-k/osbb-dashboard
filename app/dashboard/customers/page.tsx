83eb4a79962118909651962778c4296d60cb2a06
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-01-12 15:52:18 +0200

    Add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,22 @@
-export default function CustomersPage() {
-    return <p>Customers Page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+
+export default async function CustomersPage(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
+
+    return (
+        <main>
+            <CustomersTable customers={customers} />
+        </main>
+    );
 }
